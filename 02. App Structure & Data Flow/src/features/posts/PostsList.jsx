e2232295a6a3a36a9f415b369b9bab80f532da8f
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-06 18:26:49 +0600

    sort posts based on creation time

--- 02. App Structure & Data Flow/src/features/posts/PostsList.jsx
+++ 02. App Structure & Data Flow/src/features/posts/PostsList.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 function PostsList () {
   const posts = useSelector(selectAllPosts);
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-  const renderedPosts = posts.map((post) => (
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
