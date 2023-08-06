2fd739089da589c39ec5dcac82d31c9b35d5b924
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-06 14:01:30 +0600

    add AddPostsForm component to App

--- 02. App Structure & Data Flow/src/App.jsx
+++ 02. App Structure & Data Flow/src/App.jsx
@@ -1,7 +1,11 @@
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
+
 function App() {
   return (
     <main className="App">
-      Hello World
+      <AddPostForm />
+      <PostsList />
     </main>
   );
 }
