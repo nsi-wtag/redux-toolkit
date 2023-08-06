6f906ba7c83b2749a45eda0768ca4af4852c1e41
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-06 18:51:57 +0600

    add reactions to postsSlice

--- 02. App Structure & Data Flow/src/features/posts/postsSlice.js
+++ 02. App Structure & Data Flow/src/features/posts/postsSlice.js
@@ -6,13 +6,27 @@ const initialState = [
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString()
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString()
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   }
 ];
 
@@ -31,16 +45,31 @@ const postsSlice = createSlice({
             title,
             content,
             userId,
-            date: new Date().toISOString()
+            date: new Date().toISOString(),
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            }
           },
         };
       },
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+
+      if(existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    }
   },
 });
 
 export const selectAllPosts = (state) => state.posts; 
 
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
