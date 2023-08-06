a7708e86b38a7d26d06c9caba0137dae341fed7e
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-06 14:34:53 +0600

    refactor: use prepare callback to simplify invoking the action in component

--- 02. App Structure & Data Flow/src/features/posts/postsSlice.js
+++ 02. App Structure & Data Flow/src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
   {
@@ -17,10 +17,21 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload);
-    }
-  }
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content
+          },
+        };
+      },
+    },
+  },
 });
 
 export const selectAllPosts = (state) => state.posts; 
