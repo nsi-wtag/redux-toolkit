e3c216a25bbbe449c55d58f0a896ed811c190738
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-06 16:53:09 +0600

    add PostAuthor to PostsList

--- 02. App Structure & Data Flow/src/features/posts/PostsList.jsx
+++ 02. App Structure & Data Flow/src/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 
 function PostsList () {
   const posts = useSelector(selectAllPosts);
@@ -8,6 +9,9 @@ function PostsList () {
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ));
   
