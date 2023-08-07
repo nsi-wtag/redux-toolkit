97dc4e6bfdeabf2ba749fb1b8c70be6ea0acab52
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-07 16:31:13 +0600

    add react router

--- 04. Blog Project/src/index.jsx
+++ 04. Blog Project/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { store } from "./app/store.js";
 import App from "./App.jsx";
 import "./index.css";
@@ -8,6 +9,11 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
       <App />
     </Provider>
   </React.StrictMode>,
