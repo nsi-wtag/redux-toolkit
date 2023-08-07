d51561b247e066a0882746d357eee6575cdd6e9f
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-07 12:13:36 +0600

    fetch users at the first render of the app

--- 03. Async Logic & Thunks/src/index.jsx
+++ 03. Async Logic & Thunks/src/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
+import { fetchUsers } from "./features/users/usersSlice.js";
 import App from "./App.jsx";
 import "./index.css";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
