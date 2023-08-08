91722d84d5d156db67d7aea9d0f21d2bb5da0fdb
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-08 17:37:23 +0600

    dispatch fetchUsers in index

--- 04. Blog Project/src/index.jsx
+++ 04. Blog Project/src/index.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { store } from "./app/store.js";
+import { fetchUsers } from "./features/users/usersSlice.js";
 import App from "./App.jsx";
 import "./index.css";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
