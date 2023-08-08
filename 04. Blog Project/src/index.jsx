cc91c9a9d97e4d23dab31c36a471d7c2c5be48cd
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-08 17:44:55 +0600

    fetchPost is dispatched in index

--- 04. Blog Project/src/index.jsx
+++ 04. Blog Project/src/index.jsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { store } from "./app/store.js";
 import { fetchUsers } from "./features/users/usersSlice.js";
+import { fetchPosts } from "./features/posts/postsSlice.js";
 import App from "./App.jsx";
 import "./index.css";
 
 store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
