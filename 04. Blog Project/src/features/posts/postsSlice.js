9a1dc0f54acb6ac25b548c9d640fa5008d5ad469
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-08 14:28:32 +0600

    create new methods for updating posts

--- 04. Blog Project/src/features/posts/postsSlice.js
+++ 04. Blog Project/src/features/posts/postsSlice.js
@@ -20,6 +20,12 @@ export const addNewPost = createAsyncThunk(`posts/addNewPost`, async (initialPos
   return response.data;
 });
 
+export const updatePost = createAsyncThunk(`posts/updatePost`, async (initialPost) => {
+  const { id } = initialPost;
+  const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+  return response.data;
+});
+
 const postsSlice = createSlice({
   name: "posts",
   initialState,
@@ -94,6 +100,17 @@ const postsSlice = createSlice({
         };
 
         state.posts.push(action.payload);
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if(!action.payload?.id) {
+          return;
+        }
+
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = [...posts, action.payload];
       });
   }
 });
