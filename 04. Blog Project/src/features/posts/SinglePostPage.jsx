e7f03af60743f8f82a5f5ddb4cde9d21cf8dfc40
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-08 14:29:05 +0600

    add new link to editing a post in SinglePostPage

--- 04. Blog Project/src/features/posts/SinglePostPage.jsx
+++ 04. Blog Project/src/features/posts/SinglePostPage.jsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { selectPostById } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
 function SinglePostPage() {
-  const postId = useParams();
+  const { postId } = useParams();
   const post = useSelector((state) => selectPostById(state, Number(postId)));
 
   if(!post) {
@@ -22,6 +23,7 @@ function SinglePostPage() {
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <p className="postCredit">
+      <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
