f5965dd68ab4eca3944849eafc5e90b1557f5579
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-08 17:38:25 +0600

    update postAuthor

--- 04. Blog Project/src/features/posts/PostAuthor.jsx
+++ 04. Blog Project/src/features/posts/PostAuthor.jsx
@@ -3,7 +3,6 @@ import { selectAllUsers } from "../users/usersSlice";
 
 function PostAuthor({ userId }) {
   const users = useSelector(selectAllUsers);
-
   const author = users.find((user) => user.id === userId);
 
   return (
