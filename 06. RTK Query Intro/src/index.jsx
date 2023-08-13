14166541a0e1d005010d654433ec017668510af6
Naimul Islam Siam <naimul.siam@welldev.io>
2023-08-13 17:23:20 +0600

    add apiSlice to the ApiProvider

--- 06. RTK Query Intro/src/index.jsx
+++ 06. RTK Query Intro/src/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice.js";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>,
 );
